dbcb5b138398d9bb016836335216c5314a70737d
Hanno Rein <hanno@hanno-rein.de>
2017-11-16 09:49:12 -0500

    better

---
+++ src/janus.h
@@ -0,0 +1,20 @@
+#ifndef _JANUS_H_
+#define _JANUS_H_
+
+// Number of particles
+#define N 9
+
+// Scale for conversion between FP and INT
+const double scale_vel  = 1e-16; 
+const double scale_pos  = 1e-16;
+
+// INT datatype used
+#define REB_PARTICLE_INT_TYPE int64_t
+
+//#pragma SDS data access_pattern(A:SEQUENTIAL, B:SEQUENTIAL, C:SEQUENTIAL)
+void janus_run(REB_PARTICLE_INT_TYPE p_int_in[6*N], REB_PARTICLE_INT_TYPE p_int_out[6*N], double p_mass[N],long steps, double dt);
+
+
+#endif /* _JANUS_H_ */
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -43,15 +43,7 @@ ALL TIMES.
 #include <math.h>
 
 #include "sds_lib.h"
-#include "mmultadd.h"
-
-#ifndef NUM_TESTS
-#define NUM_TESTS 4
-#endif
-
-
-
-
+#include "janus.h"
 
 class perf_counter
 {
@@ -64,16 +56,13 @@ public:
      inline uint64_t avg_cpu_cycles() { return ((tot+(calls>>1)) / calls); };
 };
 
-void janus_run_golden(REB_PARTICLE_INT_TYPE *p_int_in, REB_PARTICLE_INT_TYPE *p_int_out, double* p_mass)
-{
+void janus_run_golden(REB_PARTICLE_INT_TYPE p_int_in[6*N], REB_PARTICLE_INT_TYPE p_int_out[6*N], double p_mass[N],long steps,double dt){
     REB_PARTICLE_INT_TYPE p_int[6*N];
-    double p[10*N];
+    double p[6*N];  // x y z ax ay az
     for(unsigned int i=0; i<6*N; i++){
         p_int[i] = p_int_in[i];
     }
-    double t = 0;
-    while (t<2.*M_PI*1e3){ // 1000 year
-        const double dt = 0.01;
+    for(long i=0;i<steps;i++){
         // One leapfrog step
         // drift();
         for(unsigned int i=0; i<N; i++){
@@ -84,35 +73,35 @@ void janus_run_golden(REB_PARTICLE_INT_TYPE *p_int_in, REB_PARTICLE_INT_TYPE *p_
 
         // to_double_pos();
         for(unsigned int i=0; i<N; i++){
-            p[10*i+0] = ((double)p_int[6*i+0])*scale_pos;
-            p[10*i+1] = ((double)p_int[6*i+1])*scale_pos;
-            p[10*i+2] = ((double)p_int[6*i+2])*scale_pos;
+            p[6*i+0] = ((double)p_int[6*i+0])*scale_pos;
+            p[6*i+1] = ((double)p_int[6*i+1])*scale_pos;
+            p[6*i+2] = ((double)p_int[6*i+2])*scale_pos;
         }
         // gravity();
         for(unsigned int i=0; i<N; i++){
-            p[10*i+6] = 0.;
-            p[10*i+7] = 0.;
-            p[10*i+8] = 0.;
+            p[6*i+3] = 0.;
+            p[6*i+4] = 0.;
+            p[6*i+5] = 0.;
             for(unsigned int j=0; j<N; j++){
                 if (i!=j){
-                    const double dx = p[10*i+0] - p[10*j+0];
-                    const double dy = p[10*i+1] - p[10*j+1];
-                    const double dz = p[10*i+2] - p[10*j+2];
+                    const double dx = p[6*i+0] - p[6*j+0];
+                    const double dy = p[6*i+1] - p[6*j+1];
+                    const double dz = p[6*i+2] - p[6*j+2];
                     const double _r = sqrt(dx*dx + dy*dy + dz*dz);
-                    const double prefact = -1/(_r*_r*_r)*p_mass[10*j+9];
+                    const double prefact = -1/(_r*_r*_r)*p_mass[j];
 
-                    p[10*i+6]    += prefact*dx;
-                    p[10*i+7]    += prefact*dy;
-                    p[10*i+8]    += prefact*dz;
+                    p[6*i+3]    += prefact*dx;
+                    p[6*i+4]    += prefact*dy;
+                    p[6*i+5]    += prefact*dz;
                 }
             }
         }
 
         // kick();
         for(unsigned int i=0; i<N; i++){
-            p_int[6*i+3] += (REB_PARTICLE_INT_TYPE)(dt*p[10*i+6]/scale_vel);
-            p_int[6*i+4] += (REB_PARTICLE_INT_TYPE)(dt*p[10*i+7]/scale_vel);
-            p_int[6*i+5] += (REB_PARTICLE_INT_TYPE)(dt*p[10*i+8]/scale_vel);
+            p_int[6*i+3] += (REB_PARTICLE_INT_TYPE)(dt*p[6*i+3]/scale_vel);
+            p_int[6*i+4] += (REB_PARTICLE_INT_TYPE)(dt*p[6*i+4]/scale_vel);
+            p_int[6*i+5] += (REB_PARTICLE_INT_TYPE)(dt*p[6*i+5]/scale_vel);
         }
 
         // drift();
@@ -121,91 +110,79 @@ void janus_run_golden(REB_PARTICLE_INT_TYPE *p_int_in, REB_PARTICLE_INT_TYPE *p_
             p_int[6*i+1] += (REB_PARTICLE_INT_TYPE)(dt/2.*(double)p_int[6*i+4]*scale_vel/scale_pos);
             p_int[6*i+2] += (REB_PARTICLE_INT_TYPE)(dt/2.*(double)p_int[6*i+5]*scale_vel/scale_pos);
         }
-
-        t += dt;
     }
     for(unsigned int i=0; i<6*N; i++){
         p_int_out[i] = p_int[i];
     }
 
-
 }
 
-//static int result_check(float *D, float *D_sw)
-//{
-//     for (int i = 0; i < N * N; i++) {
-//          if (D_sw[i] != D[i]) {
-//               std::cout << "Mismatch: data index=" << i << "d=" << D_sw[i] 
-//                        << ", dout=" << D[i] << std::endl;
-//               return 1;
-//          }
-//     }
-//     return 0;
-//}
+static int result_check(REB_PARTICLE_INT_TYPE p_int_sw[6*N], REB_PARTICLE_INT_TYPE p_int_hw[6*N])
+{
+     for (int i=0; i<6*N; i++){
+          if (p_int_sw[i] != p_int_hw[i]){
+               std::cout << "Mismatch: data index=" << i << "sw=" << p_int_sw[i] << ", hw=" << p_int_hw[i] << std::endl;
+               return 1;
+          }
+     }
+     return 0;
+}
 
 int janus_test(REB_PARTICLE_INT_TYPE* p_int, REB_PARTICLE_INT_TYPE* p_int_out_sw, REB_PARTICLE_INT_TYPE* p_int_out_hw)
 {
-     std::cout << "Testing " << NUM_TESTS << " iterations of " << N << "-body JANUS ..." << std::endl;
-
-     perf_counter hw_ctr, sw_ctr;
+    perf_counter hw_ctr, sw_ctr;
 	double p[] = {
-			 0.0021709922250528,  0.0057845061154043,  -0.0001290326677066,
-			-0.0003084904334499,  0.0003164862379414, 0.0000072860648107,
-            0.,0.,0.,
-			1.0000000000000000,
-			 -0.1529074277548495,  -0.4329649810759809,  -0.0217536815870956,
-			1.2130892048755062,  -0.4636664872138580, -0.1492230266727991,
-            0.,0.,0.,
-			0.0000001660114153,
-			 -0.7051385792282048,  0.1305062392874893,  0.0423980407616931,
-			-0.2107118903711193,  -1.1628741220859935, -0.0038067721592922,
-            0.,0.,0.,
-			0.0000024478382878,
-			 0.8303864923760965,  0.5551748865431479,  -0.0001556226179998,
-			-0.5694403294004744,  0.8300359440285254, -0.0000250486216637,
-            0.,0.,0.,
-			0.0000030404326480,
-			 -1.6007632981663540,  0.4507843866326728,  0.0485350310380760,
-			-0.1874661855400607,  -0.7140231189065021, -0.0103688562255236,
-            0.,0.,0.,
-			0.0000003227156038,
-			 -4.5444724195553627,  -2.9811209359531872,  0.1140115745580475,
-			0.2354668506120313,  -0.3459544002171689, -0.0038305410200901,
-            0.,0.,0.,
-			0.0009547919152112,
-			 -0.2998316596246585,  -10.0512228718170959,  0.1866942196718307,
-			0.3063599906570191,  -0.0107135147677418, -0.0120072161180579,
-            0.,0.,0.,
-			0.0002858856727222,
-			 17.8418531053445939,  8.8433796310403689,  -0.1982994964737093,
-			-0.1032131635550300,  0.1941992816066720, 0.0020584917278455,
-            0.,0.,0.,
-			0.0000436624373583,
-			 28.6228992820092181,  -8.7910334836014847,  -0.4786090163574258,
-			0.0523633993793736,  0.1755278382196959, -0.0048214129381180,
-            0.,0.,0.,
-			0.0000515138377263
-	};
+        0.0021709922250528,  0.0057845061154043,  -0.0001290326677066,
+        -0.0003084904334499,  0.0003164862379414, 0.0000072860648107,
+        -0.1529074277548495,  -0.4329649810759809,  -0.0217536815870956,
+        1.2130892048755062,  -0.4636664872138580, -0.1492230266727991,
+        -0.7051385792282048,  0.1305062392874893,  0.0423980407616931,
+        -0.2107118903711193,  -1.1628741220859935, -0.0038067721592922,
+        0.8303864923760965,  0.5551748865431479,  -0.0001556226179998,
+        -0.5694403294004744,  0.8300359440285254, -0.0000250486216637,
+        -1.6007632981663540,  0.4507843866326728,  0.0485350310380760,
+        -0.1874661855400607,  -0.7140231189065021, -0.0103688562255236,
+        -4.5444724195553627,  -2.9811209359531872,  0.1140115745580475,
+        0.2354668506120313,  -0.3459544002171689, -0.0038305410200901,
+        -0.2998316596246585,  -10.0512228718170959,  0.1866942196718307,
+        0.3063599906570191,  -0.0107135147677418, -0.0120072161180579,
+        17.8418531053445939,  8.8433796310403689,  -0.1982994964737093,
+        -0.1032131635550300,  0.1941992816066720, 0.0020584917278455,
+        28.6228992820092181,  -8.7910334836014847,  -0.4786090163574258,
+        0.0523633993793736,  0.1755278382196959, -0.0048214129381180,
+    };
+    double p_mass[] = {
+        1.0000000000000000,
+        0.0000001660114153,
+        0.0000024478382878,
+        0.0000030404326480,
+        0.0000003227156038,
+        0.0009547919152112,
+        0.0002858856727222,
+        0.0000436624373583,
+        0.0000515138377263,
+    };
 	for(unsigned int i=0; i<N; i++){
-            p_int[6*i+0] = p[10*i+0]/scale_pos;
-            p_int[6*i+1] = p[10*i+1]/scale_pos;
-            p_int[6*i+2] = p[10*i+2]/scale_pos;
-            p_int[6*i+3] = p[10*i+3]/scale_vel;
-            p_int[6*i+4] = p[10*i+4]/scale_vel;
-            p_int[6*i+5] = p[10*i+5]/scale_vel;
+        p_int[6*i+0] = p[6*i+0]/scale_pos;
+        p_int[6*i+1] = p[6*i+1]/scale_pos;
+        p_int[6*i+2] = p[6*i+2]/scale_pos;
+        p_int[6*i+3] = p[6*i+3]/scale_vel;
+        p_int[6*i+4] = p[6*i+4]/scale_vel;
+        p_int[6*i+5] = p[6*i+5]/scale_vel;
     }
 
      
-     for (int i = 0; i < NUM_TESTS; i++) 
-     {
-          sw_ctr.start();
-          janus_run_golden(p_int,p_int_out_sw,p);
-          sw_ctr.stop();
-          
-          hw_ctr.start();
-          janus_run(p_int,p_int_out_hw,p);
-          hw_ctr.stop();
-     }
+    double dt = 0.01;
+    long steps = 10000;
+
+    sw_ctr.start();
+    janus_run_golden(p_int,p_int_out_sw,p_mass,steps,dt);
+    sw_ctr.stop();
+
+    hw_ctr.start();
+    janus_run(p_int,p_int_out_hw,p_mass,steps,dt);
+    hw_ctr.stop();
+    result_check(int_out_sw,int_out_hw);
 
      uint64_t sw_cycles = sw_ctr.avg_cpu_cycles();
      uint64_t hw_cycles = hw_ctr.avg_cpu_cycles();
